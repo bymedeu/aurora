c3a8358fb3436f06e42a86cef7f946f5b383dc53
Amadéo Héaulme <amadeo.heaulme@epita.fr>
2026-08-03 22:02:08 +0200

    feat: retook m2 xor example

--- examples/xor/train.py
+++ examples/xor/train.py
@@ -16,3 +16,43 @@ Start simple:
       • Output prediction: Ŷ = σ(Z₂)
   4. Print the output shape and raw predictions to confirm matrix multiplication works.#
 """
+
+import numpy as np
+
+X = np.matrix([[0, 0], [0, 1], [1, 0], [1, 1]])
+Y = np.matrix([[0], [1], [1], [0]])
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+class Network(object):
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(x, y) for y, x in zip(sizes[:-1], sizes[1:])]
+        """
+        print(self.weights[0])
+        print(self.biases[0])
+        print(self.weights[1])
+        print(self.biases[1])
+        """
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, a) + b
+            a = sigmoid(z)
+        return a
+
+
+nw = Network([2, 3, 1])
+for row in np.asarray(X):
+    sample = row.reshape(2, 1)
+    pred = nw.feedforward(sample)
+    print(sample.T, pred)
